6e95f8d1ff819ba6df953cc28089dd6cb6f3b119
Skip Taylor <tayloraid@gmail.com>
2015-06-14 18:09:23 -0400

    fixes user routes.

--- routes/user.rb
+++ routes/user.rb
@@ -1,14 +1,14 @@
-get '/user/users/?' do
+get '/users/?' do
   @user = User.all
   erb :'/user/users'
 end
 
-get '/user/new/?' do
+get '/new-user/?' do
   @user = User.new
   erb :'/user/edit'
 end
 
-post '/user/new/?' do
+post '/new-user/?' do
   user = User.create(
     :first_name   => params[:first_name], 
     :last_name    => params[:last_name],
@@ -26,17 +26,17 @@ post '/user/new/?' do
   redirect "/user/users"
 end
 
-get '/user/user/?' do
+get '/user/?' do
   @user = User.get(params[:id])
   erb :'/user/user'
 end
 
-get '/user/:id/edit/?' do
+get '/:id/edit-user/?' do
   @user = User.get(params[:id])
   erb :'/user/edit'
 end
 
-post '/user/:id/edit/?' do
+post '/:id/edit-user/?' do
   user = User.get(params[:id])
   user.update(
     :first_name   => params[:first_name], 
@@ -55,7 +55,7 @@ post '/user/:id/edit/?' do
   redirect "/user/users"
 end
 
-get '/user/:id/delete/?' do
+get '/:id/delete-user/?' do
   user = User.get(params[:id])
   user.destroy
   redirect '/user/users'
