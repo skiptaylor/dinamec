64278dcd8cfb165e14796da5688ca165f9aaf03b
Skip Taylor <tayloraid@gmail.com>
2015-06-15 12:45:02 -0400

    adds to seed

--- data/seeds/user.rb
+++ data/seeds/user.rb
@@ -1,6 +1,6 @@
 
-User.create username: 'skip', password: 'taylor', user_type: 'Admin'
-User.create username: 'sam', password: 'wilkins', user_type: 'Admin'
+User.create username: 'skip', password: 'taylor', user_type: 'Admin', first_name: 'Skip', last_name: 'Taylor', title: 'Developer', email: 'skip@tayloraid.com', phone1: '678-294-0131', company_id: '1'
+User.create username: 'sam', password: 'wilkins', user_type: 'Admin', first_name: 'Samantha', last_name: 'Wilkins', title: 'CSR', email: 'samantha.wilkins@dinamecsystems.com', phone1: '770-421-1181 X236', company_id: '2'
 
-Company.create company: 'New Company'
+Company.create company: 'Taylor-Aid'
 Company.create company: 'Dinamec Systems'
