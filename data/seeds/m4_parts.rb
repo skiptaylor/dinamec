2ef59d2e4924cd7f2151db921238742f625254d8
Skip Taylor <tayloraid@gmail.com>
2015-07-28 08:14:21 -0400

    Fixes suggested qty for parts

--- data/seeds/m4_parts.rb
+++ data/seeds/m4_parts.rb
@@ -4,11 +4,11 @@ DataMapper.repository(:default).adapter.execute("delete from parts")
 CSV.open('./data/parts/m4.csv', { headers: true }) do |csv_lines|
   csv_lines.each do |line|
     Part.create(
-      machine_id:   '1',
-      description:  line['Description'],
-      quantity:     line['Qty'].to_i,
-      part_number:  line['KSI Part #'],
-      part:         line['F/S']
+      machine_id:     '1',
+      description:    line['Description'],
+      suggested_qty:  line['Qty'].to_i,
+      part_number:    line['KSI Part #'],
+      part:           line['F/S']
     )
   end
 end
