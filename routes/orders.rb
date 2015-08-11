9c15b2d7afa5ae0a888b53b9dc8452f84899f8c8
Skip Taylor <tayloraid@gmail.com>
2015-08-11 12:16:32 -0400

    orders email items

--- routes/orders.rb
+++ routes/orders.rb
@@ -32,7 +32,7 @@ post '/machines/:machine_id/orders/order-part/?' do
   end
   
   
-  Email.quote_request(order.id, order.comment, order.machine_id)
+  Email.quote_request(order.machine_id, order.id, order.comment)
   
   erb :"/company/company"
 end
