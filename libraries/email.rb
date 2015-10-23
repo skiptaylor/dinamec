ea765af83fa9ef582de583a541c65dd0ffa14b31
Skip Taylor <tayloraid@gmail.com>
2015-10-23 13:02:06 -0400

    Test email without parts

--- libraries/email.rb
+++ libraries/email.rb
@@ -1,20 +1,5 @@
 class Email
   
-  def self.quote_request machine_id, id, comment
-
-    body = ''
-    body << "<p>Machine: #{machine_id}</p>"
-    body << "<p>Order number: #{id}</p>"
-    body << "<p>Comment: #{comment}</p>"
-
-    Pony.mail(
-      headers: { 'Content-Type' => 'text/html' },
-      to: 'skip@tayloraid.com',
-      from: 'no-reply@dinamecsystems.com',
-      subject: 'Request for Parts Quote',
-      body: body
-    )
-  end
   
   def self.respond name, company, email, phone, comment
 
@@ -34,23 +19,6 @@ class Email
     )
   end
   
-  def self.cleaning name, company, email, phone, description
-
-    body = ''
-    body << "<p>Name: #{name}</p>"
-    body << "<p>Company: #{company}</p>"
-    body << "<p>Email: #{email}</p>"
-    body << "<p>Phone: #{phone}</p>"
-    body << "<p>Description: #{description}</p>"
-
-    Pony.mail(
-      headers: { 'Content-Type' => 'text/html' },
-      to: 'skip@tayloraid.com',
-      from: 'no-reply@dinamecsystems.com',
-      subject: 'Request for Sample Parts Cleaning',
-      body: body
-    )
-  end
 
 end
 
